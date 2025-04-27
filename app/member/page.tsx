6a895d2b1924aec1855889abfead7ee1f77d6df7
yhosutun2491 <yhosutun2491@gmail.com>
2025-04-27 15:11:28 +0800

    feat- 增加修改密碼表單

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -1,10 +1,12 @@
 import UserInfoForm from "@/components/Member/UserInfoForm"
+import ChangePasswordForm from "@/components/Member/ChangePasswordForm"
 
 export default function MemberPage() {
-    return <div className="member__info__page flex flex-col gap-[2rem]">
-      <p className="text-primary-500 text-2xl">會員個人資料</p>
-      <section className="form_wrapper w-full py-[1.5rem] grid grid-cols-2">
+    return <div className="member__info__page flex flex-col gap-[2rem] overflow-y-scroll">
+      <p className="text-primary-500 text-2xl">管理個人資料</p>
+      <section className="form_wrapper w-full py-[1.5rem] grid grid-cols-2 gap-8">
         <UserInfoForm />
+        <ChangePasswordForm />
       </section>
     </div>
 }
