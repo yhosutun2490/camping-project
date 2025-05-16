6eca0e42dc9cc4704010ece2f45ad95361416d2d
yhosutun2490 <yhosutun2491@gmail.com>
2025-05-16 13:30:52 +0800

    <modify> 調整圖片

--- sections/AboutUsSection/AboutUsCard/index.tsx
+++ sections/AboutUsSection/AboutUsCard/index.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image"
 interface Props {
   data: {
     id: string,
@@ -11,13 +11,16 @@ interface Props {
 export default function AboutUsCard({ data: { id, title, image, description } }: Props) {
 
   return (
-    < div className="card card-side border-2 border-primary-300 rounded-xl shadow-sm" >
-      <figure>
-        <img
+    < div className="about_card border-2 border-primary-300 rounded-xl shadow-sm flex items-center" >
+      <figure className="flex items-center justify-center min-w-16 h-16 bg-primary-50 rounded-full">
+        <Image
+          width={32}
+          height={32}
           src={image}
-          alt={title} />
+          alt={title}
+        />
       </figure>
-      <div className="card-body">
+      <div className="card-body text-start">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
       </div>
