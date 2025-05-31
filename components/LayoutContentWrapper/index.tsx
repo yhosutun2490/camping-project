521dcbb5f34e705c09a391b4ef421e79576bc6cb
yhosutun2491 <yhosutun2491@gmail.com>
2025-05-31 19:19:32 +0800

    fix- layoutwrapper加入換頁將滾軸至頂功能

--- components/LayoutContentWrapper/index.tsx
+++ components/LayoutContentWrapper/index.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 import clsx from 'clsx';
 
 export default function LayoutContentWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isHome = pathname === '/';
+
+  useEffect(() => {
+    const container = document.getElementById('main-scroll-container');
+    container?.scrollTo({ top: 0, behavior: 'instant' });
+  }, [pathname]); // 每次切換頁面時觸發
 
   return (
     <main
